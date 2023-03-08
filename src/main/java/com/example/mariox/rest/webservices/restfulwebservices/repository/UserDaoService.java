6bc4ec046a759a2cd8a738f82d6b31924fe102ca
Himanshu Singh <61236944+himanshusingh335@users.noreply.github.com>
2023-03-08 09:34:54 +0530

    remove unnecessary @repository

--- src/main/java/com/example/mariox/rest/webservices/restfulwebservices/repository/UserDaoService.java
+++ src/main/java/com/example/mariox/rest/webservices/restfulwebservices/repository/UserDaoService.java
@@ -1,12 +1,10 @@
 package com.example.mariox.rest.webservices.restfulwebservices.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.example.mariox.rest.webservices.restfulwebservices.model.User;
 
 
-@Repository
 public interface UserDaoService extends JpaRepository<User, Integer> {
 
 	}
